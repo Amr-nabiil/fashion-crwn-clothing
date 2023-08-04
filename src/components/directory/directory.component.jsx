1ea7e728b0f7d5eb88ab23eaca94ab87fd82d249
Amr-nabiil <armtntat@gmail.com>
2023-08-04 21:27:15 +0200

    ubdate our menu-item to navigate withrouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,29 +13,34 @@ class Directory extends React.Component {
             {
              title: "hats",
              imageUrl:"https://www.bon-clic-bon-genre.eu/image/image/visage-rond.jpg",
-             id: 1
+             id: 1,
+             linkUrl: 'hats'
             },
             {
              title: "jackets",
              imageUrl:"https://nypost.com/wp-content/uploads/sites/2/2022/03/Best-Denim-Jackets-for-Women.jpg?resize=878,585&quality=75&strip=all",
-             id: 2   
+             id: 2  ,
+             linkUrl: '' 
             },
             {
              title: "sneakers",
              imageUrl: "https://www.golfposer.com/media/magefan_blog/Nike-Air-Max-90-Flash-Lime-Golf-Shoe-Hero.jpg",
-             id: 3    
+             id: 3    ,
+             linkUrl: ''
             },
             {
              title: "womens",
              imageUrl:"https://media.allure.com/photos/57dc1a877b47312c0ad83725/16:9/w_2560%2Cc_limit/model%2520jcrew.jpg",
              size:"large",
-             id: 4    
+             id: 4   ,
+             linkUrl: '' 
             },
             {
              title: "mens",
              imageUrl:"https://en.svoboda-williams.com/images/lifestyle_article/1200x630/99546223-resize~fashion-for-men.jpg",
              size:"large",
-             id: 5
+             id: 5,
+             linkUrl: ''
             }    
             ]
         }
@@ -43,8 +48,8 @@ class Directory extends React.Component {
     render() {
         return(
             <div className="directory-menu">
-                {this.state.sections.map(({ title, imageUrl ,id , size  } ) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                {this.state.sections.map(({ id , ...otherSectionProps} ) => (
+                    <MenuItem key={id} {...otherSectionProps} />
                 )) }
             </div>
         )
