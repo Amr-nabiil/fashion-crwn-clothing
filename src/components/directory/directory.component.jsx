d85f1f095924333f61fd56b5b6cba374517c4780
Amr-nabiil <armtntat@gmail.com>
2023-08-12 19:48:16 +0200

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,58 +1,23 @@
-import React from "react";
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-import MenuItem from "../menu-item/menu-item.component";
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
-import "./directory.styles.scss";
+import MenuItem from '../menu-item/menu-item.component';
 
+import './directory.styles.scss';
 
-class Directory extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            sections: [
-            {
-             title: "hats",
-             imageUrl:"https://www.bon-clic-bon-genre.eu/image/image/visage-rond.jpg",
-             id: 1,
-             linkUrl: 'hats'
-            },
-            {
-             title: "jackets",
-             imageUrl:"https://nypost.com/wp-content/uploads/sites/2/2022/03/Best-Denim-Jackets-for-Women.jpg?resize=878,585&quality=75&strip=all",
-             id: 2  ,
-             linkUrl: '' 
-            },
-            {
-             title: "sneakers",
-             imageUrl: "https://www.golfposer.com/media/magefan_blog/Nike-Air-Max-90-Flash-Lime-Golf-Shoe-Hero.jpg",
-             id: 3    ,
-             linkUrl: ''
-            },
-            {
-             title: "womens",
-             imageUrl:"https://media.allure.com/photos/57dc1a877b47312c0ad83725/16:9/w_2560%2Cc_limit/model%2520jcrew.jpg",
-             size:"large",
-             id: 4   ,
-             linkUrl: '' 
-            },
-            {
-             title: "mens",
-             imageUrl:"https://en.svoboda-williams.com/images/lifestyle_article/1200x630/99546223-resize~fashion-for-men.jpg",
-             size:"large",
-             id: 5,
-             linkUrl: ''
-            }    
-            ]
-        }
-    }
-    render() {
-        return(
-            <div className="directory-menu">
-                {this.state.sections.map(({ id , ...otherSectionProps} ) => (
-                    <MenuItem key={id} {...otherSectionProps} />
-                )) }
-            </div>
-        )
-    }
-}
-export default Directory;
+const Directory = ({ sections }) => (
+  <div className='directory-menu'>
+    {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
+
+export default connect(mapStateToProps)(Directory);
--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import store from './redux/store';
+import { persistor }  from './redux/store';
 import './index.css';
 import App from './App';
 
@@ -13,7 +16,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <Provider store={store}>
     <BrowserRouter>
+    <PersistGate persistor={persistor}>
         <App />
+      </PersistGate>
     </BrowserRouter>
     </Provider>
 );
